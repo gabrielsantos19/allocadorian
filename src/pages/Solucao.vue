a1f4f07cdcb3c4255f8b0e9f8aeb7d44c0d4a731
gabrielsantos19 <25865943+gabrielsantos19@users.noreply.github.com>
2021-05-28 15:02:34 -0300

    Adicionar filtros

--- src/pages/Solucao.vue
+++ src/pages/Solucao.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="flex" style="overflow: auto;">
+  <div class="solucao">
+    <div class="barra">
+      <span class="titulo">Solução</span>
+      <button @click="personalizar">
+        Personalizar
+      </button>
+      <input
+        class="filtro" 
+        placeholder="filtrar vetores" />
+
+      <button v-for="(i, index) in agrupamento" :key="i"
+          :id="index"
+          draggable="true"
+          @dragstart="agrupamentoDragstart"
+          @drop="agrupamentoDrop"
+          @dragover="agrupamentoDragover">
+        {{conjuntos[i].descricao.nome}}
+      </button>
+    </div>
+
     <grafico-component v-if="solucao"
-      :agrupamento="[2,1,0]"
-      :vetores="solucao.vetores" />
+      :agrupamento="agrupamento"
+      :vetores="solucao.vetores" 
+      class="grafico"/>
   </div>
 </template>
 
@@ -10,6 +30,7 @@
 import * as ConjuntosDAO from 'src/lib/DAO/conjuntosDAO.js'
 import * as VetoresDAO from 'src/lib/DAO/vetoresDAO.js'
 import * as SolucoesDAO from 'src/lib/DAO/solucoesDAO.js'
+import * as PersonalizadaDAO from 'src/lib/DAO/personalizadaDAO.js'
 
 import * as Conjuntos from 'src/lib/conjuntos.js'
 import * as Vetores from 'src/lib/vetores.js'
@@ -30,8 +51,33 @@ export default {
 
       conjuntos: null,
       vetores: null,
-      solucoes: null
+      solucoes: null,
+
+      agrupamento: null,
     }
+  },
+  methods: {
+    personalizar () {
+      PersonalizadaDAO.post(this.solucao)
+      .then(this.$router.push('/personalizada'))
+    },
+    agrupamentoDragstart (ev) {
+      ev.dataTransfer.setData('text', ev.target.id)
+    },
+    agrupamentoDrop (event) {
+      event.preventDefault()
+      const origem = event.dataTransfer.getData("text")
+      const destino = event.target.id
+
+      const agrup = this.agrupamento.slice()
+      const temp = agrup[origem]
+      agrup[origem] = agrup[destino]
+      agrup[destino] = temp
+      this.agrupamento = agrup
+    },
+    agrupamentoDragover (ev) {
+      ev.preventDefault()
+    },
   },
   mounted () {
     const id = this.$route.query.id
@@ -61,10 +107,49 @@ export default {
         this.solucaoId = id
         this.solucao = a[2][id]
       }
+
+      const agrup = []
+      for (let i=0; i<a[0].length; ++i) {
+        agrup.push(i)
+      }
+      this.agrupamento = agrup
     })
   }
 }
 </script>
 
 <style scoped>
+.solucao {
+  display: flex;
+  flex-flow: column nowrap;
+  overflow: auto;
+}
+.barra {
+  display: flex;
+  flex-flow: row nowrap;
+  flex-grow: 1;
+  min-height: 42px;
+  align-items: center;
+  color: white;
+  background-color: rgb(40,40,40);
+  overflow: auto;
+}
+.titulo {
+  flex-shrink: 0;
+  font-size: 22px;
+  padding: 0px 15px 0px 15px;
+  overflow: hidden;
+}
+.filtro {
+  flex-grow: 1;
+  max-width: 500px;
+  min-width: 100px;
+  margin: 0px 10px;
+}
+.grafico {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  overflow: auto;
+}
 </style>
