412364aee1e003643d65fa30f36fa6ac2af1db53
gabrielsantos19 <25865943+gabrielsantos19@users.noreply.github.com>
2021-05-27 21:10:26 -0300

    Melhorar visualização de cada Vetor nas listagens

--- src/components/Vetor.vue
+++ src/components/Vetor.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container" :title="JSON.stringify(vetor)">
+  <div class="vetor" :title="JSON.stringify(vetor)">
     <div class="objetos">
-      <div v-for="(objeto, index) in texto" :key="index">
+      <div v-for="(objeto, index) in texto" :key="index" class="objeto">
         {{objeto}}
       </div>
     </div>
@@ -25,8 +25,12 @@ export default {
       let valores = []
       for (let i=0; i<this.vetor.objetos.length; ++i) {
         const objeto = this.vetor.objetos[i]
-        const key = Object.keys(objeto)[0]
-        valores.push(objeto[key])
+        const keys3 = Object.keys(objeto).slice(0, 3)
+        const resultado = []
+        for (let k=0; k<keys3.length; ++k) {
+          resultado.push(String(objeto[keys3[k]]))
+        }
+        valores.push(resultado.join(' · '))
       }
       return valores
     }
@@ -35,26 +39,36 @@ export default {
 </script>
 
 <style scoped>
-.container {
+.vetor {
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
   align-items: center;
   
-  width: 250px;
+  width: 270px;
   font-size: 15px;
-  color: aliceblue;
-  background: #1f4068;
-  font-weight: bold;
+  color: white;
+  background: #027d50;
   border-radius: 7px;
   padding: 5px 15px;
   overflow: hidden;
 }
 .objetos {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
   flex-grow: 1;
+  overflow: auto;
+}
+.objeto {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 .pontuacao {
-  padding: 5px;
-  font-size: 30px;
+  padding-left: 15px;
+  font-size: 25px;
+  font-weight: bold;
+  text-align: center;
 }
 </style>
