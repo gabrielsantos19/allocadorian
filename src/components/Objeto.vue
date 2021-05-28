d59667482b3338ed222df20fbdb437804f024790
gabrielsantos19 <25865943+gabrielsantos19@users.noreply.github.com>
2021-05-27 21:08:15 -0300

    Melhorar estilo da visualização de Objeto

--- src/components/Objeto.vue
+++ src/components/Objeto.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="container">
-    <div class="titulo">{{Object.values(objeto)[0]}}</div>
-    <div class="descricao">{{Object.values(objeto)[1]}}</div>
-    <button @click="$emit('remover')">Remover</button>
-    <button @click="$emit('editar')">Editar</button>
+    <span class="titulo" :title="JSON.stringify(objeto)">
+      {{valores.length > 0 ? valores[0] : 'Vazio'}}
+    </span>
+    <div>
+      <span v-if="valores.length > 1" class="descricao">
+        {{valores[1]}}
+      </span>
+      <span v-if="valores.length > 2" class="descricao">
+        · {{valores[2]}}
+      </span>
+    </div>
+    <div>
+      <button @click="$emit('editar')">Editar</button>
+      <button @click="$emit('remover')">Remover</button>
+    </div>
   </div>
 </template>
 
@@ -15,20 +26,33 @@ export default {
       type: Object,
       required: true
     }
+  },
+  computed: {
+    valores () {
+      const values = Object.values(this.objeto)
+      const resultados = values.slice(0, 3)
+      return resultados
+    }
   }
 }
 </script>
 
 <style scoped>
 .container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+
   width: 250px;
-  height: 110px;
-  background-color: #e84545;
+  min-height: 115px;
+  color: white;
+  background-color: #c41b1b;
   border-radius: 7px;
-  padding: 10px 15px;
+  padding: 5px 15px;
   overflow: hidden;
 }
 .titulo {
+  flex-grow: 1;
   font-size: 24pt;
   white-space: nowrap;
   overflow: hidden;
