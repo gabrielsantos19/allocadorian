61c66ccdb105673c86e0584ed88ccd5e1b267d3b
gabrielsantos19 <25865943+gabrielsantos19@users.noreply.github.com>
2021-05-29 00:15:11 -0300

    Reestruturar funcionalidade e estilo

--- src/pages/Conjuntos.vue
+++ src/pages/Conjuntos.vue
@@ -1,58 +1,59 @@
 <template>
   <main class="row no-wrap col" style="overflow: auto;">
-    <div class="column no-wrap col" style="min-width: 400px;">
-      <div class="row">
-        <div class="row no-wrap q-my-sm" style="overflow: auto;">
-          <div v-for="(tabela, index) in tabelasParsed" 
-              :key="index"
-              class="column">
-            <button @click="setTabelaAtual(index)">
-              {{ tabela.descricao.nome }}
+    <div class="conjuntos">
+      <div class="barra">
+        <span>Conjuntos</span>
+        <div v-for="(c, index) in conjuntosParsed"
+            :key="index">
+          <button @click="setIndexAtual(index)">
+            {{ c.descricao.nome }}
+          </button>
+        </div>
+        <button @click="criarConjunto">
+          Novo conjunto
+        </button>
+      </div>
+
+      <div v-if="conjuntoParsed" class="cabecalho">
+        <div style="font-size: 55px" class="q-px-md">
+          {{conjuntoParsed.descricao.nome}}
+        </div>
+        <div class="column">
+          <div>
+            {{conjuntoParsed.objetos.length}} objetos
+          </div>
+          <div class="row">
+            <button @click="criarObjeto(indexAtual)">
+              Novo objeto
             </button>
-            <button @click="removerTabela(index)">
-              Remover {{ tabela.descricao.nome }}
+            <button @click="editarObjetoBase(indexAtual)">
+              Editar objeto base
+            </button>
+            <button @click="editarDescricao(indexAtual)">
+              Editar descricao
+            </button>
+            <button @click="removerConjunto(indexAtual)">
+              Remover conjunto
             </button>
           </div>
-          <button @click="criarTabela">
-            Novo conjunto
-          </button>
         </div>
       </div>
 
-      <div v-if="tabelaParsed" class="row" style="align-items: center;">
-        <div style="font-size: 55px" class="q-px-md">
-          {{tabelaParsed.descricao.nome}}
-        </div>
-        <div class="q-px-md">
-          {{tabelaParsed.objetos.length}} objetos
-        </div>
-        <div>
-          <button @click="editarObjetoBase">
-            Editar objeto base
-          </button>
-        </div>
-        <div>
-          <button @click="criarObjeto">
-            Novo objeto
-          </button>
-        </div>
-      </div>
-
-      <div v-if="tabelaParsed" class="objetos">
-        <objeto-component v-for="(linha, index) in tabelaParsed.objetos" 
+      <div v-if="conjuntoParsed" class="objetos">
+        <objeto-component v-for="(linha, index) in conjuntoParsed.objetos" 
           class="objeto"
           :key="index"
           :objeto="linha"
-          @editar="editarObjeto(index)"
-          @remover="removerObjeto(index)" />
+          @editar="editarObjeto(indexAtual, index)"
+          @remover="removerObjeto(indexAtual, index)" />
       </div>
     </div>
 
-    <div v-if="editorAberto" class="column q-px-sm q-pb-sm" style="background-color: rgb(150,150,150); min-width: 480px;">
-      <div class="row">
-        <div>{{editorTitulo}}</div>
+    <div v-if="editorAberto" class="editor">
+      <div class="barra">
         <button @click="editorSalvar(editorTexto)">Salvar</button>
-        <button @click="cancelarEdicao()" style="align-self: end">Cancelar</button>
+        <button @click="cancelarEdicao">Cancelar</button>
+        <span>{{editorTitulo}}</span>
       </div>
       <div class="column col">
         <textarea v-model="editorTexto" class="col" />
@@ -66,6 +67,7 @@ import * as ConjuntosDAO from 'src/lib/DAO/conjuntosDAO.js'
 
 import * as JSP from 'src/lib/JSP.js'
 import * as Template from 'src/lib/template.js'
+import * as Conjunto from 'src/lib/conjunto.js'
 import * as Conjuntos from 'src/lib/conjuntos.js'
 
 import ObjetoComponent from 'src/components/Objeto.vue'
@@ -75,119 +77,134 @@ export default {
   components: { 
     ObjetoComponent
   },
-  name: 'Conjuntos',
+  name: 'ConjuntosView',
   data () {
     return {
-      // Tabela atualmente em uso
-      tabela: null,
-      tabelaParsed: null,
+      indexAtual: null,
 
-      tabelas: [],
-      tabelasParsed: [],
+      conjuntos: null,
+      conjuntosParsed: null,
 
       editorAberto: true,
       editorTitulo: 'Nada selecionado',
       editorTexto: '',
-      editorSalvar: (text) => null
+      editorSalvar: (raw) => null,
+    }
+  },
+  computed: {
+    conjunto () {
+      if (this.conjuntos != null && this.indexAtual != null) {
+        return this.conjuntos[this.indexAtual]
+      }
+      return null
+    },
+    conjuntoParsed () {
+      if (this.conjuntos != null && this.indexAtual != null) {
+        return this.conjuntosParsed[this.indexAtual]
+      }
+      return null
     }
   },
   methods: {
-    criarTabela () {
-      this.editorAberto = true
-      this.editorTitulo = 'Novo conjunto'
-      this.editorTexto = Template.descricao
-      this.editorSalvar = this.inserirTabela
-    },
-    criarObjeto () {
-      this.editorAberto = true
-      this.editorTitulo = `${this.tabelaParsed.descricao.nome} > Novo objeto`
-      this.editorTexto = Template.objeto
-      this.editorSalvar = this.inserirObjeto
-    },
-    inserirTabela (descricao) {
-      const tabela = {
-        descricao: descricao,
-        objetoBase: Template.objetoBase,
-        objetos: []
-      }
-      const tabelaParsed = {
-        descricao: JSP.parse(tabela.descricao),
-        objetoBase: JSP.parse(tabela.objetoBase),
-        objetos: []
-      }
-
-      const lengthParsed = this.tabelasParsed.push(tabelaParsed)
-      const length = this.tabelas.push(tabela)
-      this.salvarTabelas()
-      this.setTabelaAtual(length-1)
-      this.cancelarEdicao()
-    },
-    inserirObjeto (textoObjeto) {
-      const objeto = JSP.parse(textoObjeto)
-      this.tabelaParsed.objetos.push(objeto)
-      this.tabela.objetos.push(textoObjeto)
-      this.salvarTabelas()
-    },
-    editarTabela () {
-
-    },
-    editarObjeto (index) {
-      this.editorAberto = true
-      this.editorTitulo = `${this.tabelaParsed.descricao.nome} > ${Object.values(this.tabelaParsed.objetos[index])[0]}`
-      this.editorTexto = this.tabela.objetos[index]
-      this.editorSalvar = (texto) => this.atualizarObjeto(index, texto)
-    },
-    editarObjetoBase () {
-      this.editorTitulo = `${this.tabelaParsed.descricao.nome} > Objeto base`
-      this.editorTexto = this.tabela.objetoBase
-      this.editorSalvar = this.atualizarObjetoBase
-    },
-    atualizarObjeto (index, texto) {
-      this.tabelaParsed.objetos[index] = JSP.parse(texto)
-      this.tabela.objetos[index] = texto
-      this.salvarTabelas()
-    },
-    atualizarObjetoBase (textoObjeto) {
-      this.tabelaParsed.objetoBase = JSP.parse(textoObjeto)
-      this.tabela.objetoBase = textoObjeto
-      this.salvarTabelas()
-    },
-    removerTabela (index) {
-      this.tabela = null
-      this.tabelaParsed = null
-      this.tabelas.splice(index, 1)
-      this.tabelasParsed.splice(index, 1)
-      this.cancelarEdicao()
-      this.salvarTabelas()
-    },
-    removerObjeto (index) {
-      this.tabelaParsed.objetos.splice(index, 1)
-      this.tabela.objetos.splice(index, 1)
-      this.cancelarEdicao()
-      this.salvarTabelas()
-    },
-    salvarTabelas () {
-      localStorage.setItem('conjuntos', JSON.stringify(this.tabelas))
-    },
-    setTabelaAtual (index) {
-      this.tabela = this.tabelas[index]
-      this.tabelaParsed = this.tabelasParsed[index]
+    setIndexAtual (index) {
+      this.indexAtual = index
     },
     cancelarEdicao () {
-      this.editorTitulo = 'Nada selecionado',
-      this.editorTexto = '',
-      this.editorSalvar = (i) => null
-    }
+      this.editorTitulo = 'Nada selecionado'
+      this.editorTexto = ''
+      this.editorSalvar = (raw) => null
+    },
+
+    criarConjunto () {
+      this.editorTitulo = 'Novo conjunto'
+      this.editorTexto = Template.descricao
+      this.editorSalvar = this.inserirConjunto
+    },
+    inserirConjunto (descricaoRaw) {
+      const novo = {
+        descricao: descricaoRaw,
+        objetoBase: Template.objetoBase,
+        objetos: [],
+      }
+      const parsed = Conjunto.parse(novo)
+      this.conjuntos.push(novo)
+      const index = this.conjuntosParsed.push(parsed)
+      this.indexAtual = index-1
+      ConjuntosDAO.post(this.conjuntos)
+    },
+    removerConjunto (conjuntoI) {
+      this.conjuntos.splice(conjuntoI, 1)
+      this.conjuntosParsed.splice(conjuntoI, 1)
+      this.indexAtual = null
+      ConjuntosDAO.post(this.conjuntos)
+    },
+
+    editarDescricao (conjuntoI) {
+      const conjunto = this.conjuntos[conjuntoI]
+      const parsed = this.conjuntosParsed[conjuntoI]
+      this.editorTitulo = `${parsed.descricao.nome} > Descrição`
+      this.editorTexto = conjunto.descricao
+      this.editorSalvar = (raw) => this.atualizarDescricao(raw, conjuntoI)
+    },
+    atualizarDescricao (raw, conjuntoI) {
+      this.conjuntos[conjuntoI].descricao = raw
+      this.conjuntosParsed[conjuntoI].descricao = JSP.parse(raw)
+      ConjuntosDAO.post(this.conjuntos)
+    },
+
+    editarObjetoBase (conjuntoI) {
+      const conjunto = this.conjuntos[conjuntoI]
+      const parsed = this.conjuntosParsed[conjuntoI]
+      this.editorTitulo = `${parsed.descricao.nome} > Objeto base`
+      this.editorTexto = conjunto.objetoBase
+      this.editorSalvar = (raw) => this.atualizarObjetoBase(raw, conjuntoI)
+    },
+    atualizarObjetoBase (raw, conjuntoI) {
+      this.conjuntos[conjuntoI].objetoBase = raw
+      this.conjuntosParsed[conjuntoI].objetoBase = JSP.parse(raw)
+      ConjuntosDAO.post(this.conjuntos)
+    },
+
+    criarObjeto (conjuntoI) {
+      const conjunto = this.conjuntosParsed[conjuntoI]
+      this.editorTitulo = `${conjunto.descricao.nome} > Novo objeto`
+      this.editorTexto = Template.objeto
+      this.editorSalvar = (raw) => this.inserirObjeto(raw, conjuntoI)
+    },
+    inserirObjeto (raw, conjuntoI) {
+      const parsed = JSP.parse(raw)
+      this.conjuntos[conjuntoI].objetos.push(raw)
+      this.conjuntosParsed[conjuntoI].objetos.push(parsed)
+      ConjuntosDAO.post(this.conjuntos)
+    },
+    editarObjeto (conjuntoI, objetoI) {
+      const conjunto = this.conjuntoParsed
+      const objeto = conjunto.objetos[objetoI]
+      this.editorTitulo = `${conjunto.descricao.nome} > ${Object.values(objeto)[0]}`
+      this.editorTexto = this.conjuntos[conjuntoI].objetos[objetoI]
+      this.editorSalvar = (raw) => this.atualizarObjeto(raw, conjuntoI, objetoI)
+    },
+    atualizarObjeto (raw, conjuntoI, objetoI) {
+      this.conjuntos[conjuntoI].objetos[objetoI] = raw
+      this.conjuntosParsed[conjuntoI].objetos[objetoI] = JSP.parse(raw)
+      ConjuntosDAO.post(this.conjuntos)
+    },
+    removerObjeto (conjuntoI, objetoI) {
+      this.conjuntos[conjuntoI].objetos.splice(objetoI, 1)
+      this.conjuntosParsed[conjuntoI].objetos.splice(objetoI, 1)
+      ConjuntosDAO.post(this.conjuntos)
+    },
   },
   mounted () {
     ConjuntosDAO.get()
     .then(conjuntos => {
-      this.tabelas = conjuntos
+      this.conjuntos = conjuntos
 
       Conjuntos.parse(conjuntos)
       .then(parsed => {
-        this.tabelasParsed = parsed
-        this.setTabelaAtual(0)
+        this.conjuntosParsed = parsed
+        if (parsed.length > 0)
+          this.setIndexAtual(0)
       })
     })
   }
@@ -195,13 +212,47 @@ export default {
 </script>
 
 <style scoped>
+.conjuntos {
+  display: flex;
+  flex-flow: column nowrap;
+  flex-grow: 1;
+  min-width: 400px;
+}
+.barra {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: center;
+  color: white;
+  min-height: 42px;
+  max-height: 42px;
+  flex-shrink: 0;
+  background-color: rgb(40,40,40);
+}
+
+.cabecalho {
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+  padding-bottom: 10px;
+}
+
 .objetos {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   overflow: auto;
+  padding: 10px 0px;
+  box-shadow: 0 1px 3px 0 grey inset;
 }
 .objeto {
   margin: 0px 0px 5px 10px;
+}
+
+.editor {
+  display: flex;
+  flex-flow: column;
+  min-width: 550px;
+  background-color: rgb(150,150,150);
 }
 </style>
